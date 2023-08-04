fbfd128a32322788bc182dbcb4b4fca77c32c7b6
L-Eval <141233954+L-Eval@users.noreply.github.com>
2023-08-05 01:03:01 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,152 +1,70 @@
-// // 使用Papa Parse从CSV文件中获取数据
-// Papa.parse("exam_LEval_leaderboard.csv", {
-//   download: true,
-//     header: true,
-//     dynamicTyping: true,
-//     complete: function(results) {
-//         let data = results.data;
-//
-//         // 获取HTML元素
-//         let table = document.getElementById('csvTable');
-//         let headerRow = table.tHead.rows[0];
-//         let tableBody = table.tBodies[0];
-//
-//         // 找出每列的最大值
-//         let columnMaxValues = {};
-//         for (let key in data[0]) {
-//             if (isNaN(data[0][key])) continue; // 如果该列不是数字，跳过
-//             columnMaxValues[key] = Math.max(...data.map(row => parseFloat(row[key])));
-//         }
-//
-//         // 创建表头
-//         let idx = 0;
-//         for (let key in data[0]) {
-//             let th = document.createElement('th');
-//             th.textContent = key;
-//             th.addEventListener('mouseover', function() {
-//                 highlightColumn(key);
-//             });
-//             th.addEventListener('mouseout', function() {
-//                 unhighlightColumn(key);
-//             });
-//             th.addEventListener('click', function() {
-//                 sortTableByColumn(table, idx);
-//             });
-//             headerRow.appendChild(th);
-//             idx++;
-//         }
-//
-//         // 创建表格主体
-//         for (let i = 0; i < data.length; i++) {
-//             let row = document.createElement('tr');
-//             for (let key in data[i]) {
-//                 let td = document.createElement('td');
-//                 td.classList.add(key); // 添加类名以便于高亮整列
-//                 if (typeof data[i][key] === 'number') {
-//                     // 如果是数字，则保留两位小数
-//                     td.textContent = data[i][key].toFixed(2);
-//                 } else {
-//                     td.textContent = data[i][key];
-//                 }
-//                 if (data[i][key] == columnMaxValues[key]) {
-//                     // 如果是最大值，则加粗显示
-//                     td.classList.add('max-value');
-//                 }
-//                 row.appendChild(td);
-//             }
-//             tableBody.appendChild(row);
-//         }
-//     }
-// });
-//
-// // 高亮整列
-// function highlightColumn(columnName) {
-//     let cells = document.getElementsByClassName(columnName);
-//     for (let i = 0; i < cells.length; i++) {
-//         cells[i].classList.add('highlight');
-//     }
-// }
-//
-// // 取消高亮整列
-// function unhighlightColumn(columnName) {
-//     let cells = document.getElementsByClassName(columnName);
-//     for (let i = 0; i < cells.length; i++) {
-//         cells[i].classList.remove('highlight');
-//     }
-// }
-//
-// // 排序表格
-// function sortTableByColumn(table, column, asc = true) {
-//     const dirModifier = asc ? 1 : -1;
-//     const tBody = table.tBodies[0];
-//     const rows = Array.from(tBody.querySelectorAll("tr"));
-//
-//     // 检查列是否为数字
-//     const isNumeric = rows.every(row => !isNaN(row.querySelectorAll("td")[column].textContent));
-//
-//     if (isNumeric) {
-//         const sortedRows = rows.sort((a, b) => {
-//             const aColText = parseFloat(a.querySelectorAll("td")[column].textContent.trim()) || 0;
-//             const bColText = parseFloat(b.querySelectorAll("td")[column].textContent.trim()) || 0;
-//
-//             return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
-//         });
-//
-//         while (tBody.firstChild) {
-//             tBody.removeChild(tBody.firstChild);
-//         }
-//
-//         sortedRows.forEach(tr => tBody.appendChild(tr));
-//
-//         // 记住当前的排序方式
-//         table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
-//         table.querySelector(`th:nth-child(${ column + 1 })`).classList.toggle("th-sort-asc", asc);
-//         table.querySelector(`th:nth-child(${ column + 1 })`).classList.toggle("th-sort-desc", !asc);
-//     }
-// }
+const urls = {
+    'ExactMatch': 'exam_LEval_leaderboard.csv',
+    'LLMJudge': 'llm_LEval_leaderboard.csv',
+    'GPT3.5Judge': 'turbo_LEval_leaderboard.csv',
+    'F1': 'f1_LEval_leaderboard.csv',
+    'ROUGE': 'rouge_LEval_leaderboard.csv'
+}
 
-Papa.parse("exam_LEval_leaderboard.csv", {
-    download: true,
-    header: true,
-    dynamicTyping: true,
-    complete: function(results) {
-        let data = results.data;
+const examRadio = document.getElementById('ExactMatch');
+const llmRadio = document.getElementById('LLMJudge');
+const gpt35Radio = document.getElementById('GPT3.5Judge');
+const f1Radio = document.getElementById('F1');
+const rougeRadio = document.getElementById('ROUGE');
+let table = document.getElementById('csvTable');
+let currentUrl = urls['ExactMatch'];
 
-        // 获取HTML元素
-        let table = document.getElementById('csvTable');
-        let headerRow = table.tHead.rows[0];
-        let tableBody = table.tBodies[0];
-        // 创建表头
-        for (let key in data[0]) {
-            let th = document.createElement('th');
-            th.textContent = key;
-            th.dataset.column = key;
-            th.addEventListener('click', function() {
-                sortTableByColumn(key, th);
-            });
-            th.addEventListener('mouseover', function() {
-                highlightColumn(key);
-            });
-            th.addEventListener('mouseout', function() {
-                unhighlightColumn(key);
-            });
-
-            headerRow.appendChild(th);
-        }
-        // 创建表格主体
-        for (let i = 0; i < data.length; i++) {
-            let row = document.createElement('tr');
-            for (let key in data[i]) {
-                let td = document.createElement('td');
-                td.textContent = data[i][key];
-                row.appendChild(td);
-            }
-            tableBody.appendChild(row);
-        }
-        highlightMaxValues(table);
-
+function updateTable(csv) {
+    // 删除现有的表头
+    while (table.tHead.rows[0].cells.length > 0) {
+        table.tHead.rows[0].deleteCell(0);
     }
-});
+    // 删除现有的表格内容
+    while (table.tBodies[0].rows.length > 0) {
+        table.tBodies[0].deleteRow(0);
+    }
+    Papa.parse(csv, {
+        download: true,
+        header: true,
+        dynamicTyping: true,
+        complete: function (results) {
+            let data = results.data;
+
+            // 获取HTML元素
+            let headerRow = table.tHead.rows[0];
+            let tableBody = table.tBodies[0];
+            // 创建表头
+            for (let key in data[0]) {
+                let th = document.createElement('th');
+                th.textContent = key;
+                th.dataset.column = key;
+                th.addEventListener('click', function () {
+                    sortTableByColumn(key, th);
+                });
+                th.addEventListener('mouseover', function () {
+                    highlightColumn(key);
+                });
+                th.addEventListener('mouseout', function () {
+                    unhighlightColumn(key);
+                });
+
+                headerRow.appendChild(th);
+            }
+            // 创建表格主体
+            for (let i = 0; i < data.length; i++) {
+                let row = document.createElement('tr');
+                for (let key in data[i]) {
+                    let td = document.createElement('td');
+                    td.textContent = data[i][key];
+                    row.appendChild(td);
+                }
+                tableBody.appendChild(row);
+            }
+            highlightMaxValues(table);
+
+        }
+    });
+}
 
 // 标记每列的最大值
 function highlightMaxValues(table) {
@@ -167,6 +85,21 @@ function highlightMaxValues(table) {
             });
         }
     });
+}
+
+
+
+function updateCautionMessage(evaluator) {
+    let cautionText;
+    if (evaluator === 'LLMJudge') {
+        cautionText = 'GPT4 and GPT3.5 may favor models with longer outputs and/or those that were fine-tuned on their outputs.';
+    }
+     else if (evaluator === 'ROUGE') {
+        cautionText = 'ROUGE and F1 score may favor predictions with similar length with the ground truth.';
+    }else if (evaluator === 'ExactMatch') {
+        cautionText = '1. EM score is easily affected by prompt engineering if it can reduce the difficulty of extracting the correct answer from the outputs\n 2. please also notice that The topic retrieval score is only comparable when the number of input tokens is the same';
+    }
+    document.getElementById('caution').innerText = 'Caution:' + cautionText + "\n" + "*Hint: if you click on a column header, the rows in the table will be re-ordered based on the values in that column.";
 }
 
 
@@ -217,3 +150,40 @@ function sortTableByColumn(column, th) {
     // 记住当前的排序方式
     th.dataset.order = th.dataset.order === 'asc' ? 'desc' : 'asc';
 }
+
+
+
+updateTable(urls['ExactMatch']);
+
+examRadio.addEventListener('click', function () {
+    currentUrl = urls['ExactMatch'];
+    updateTable(currentUrl);
+    updateCautionMessage('ExactMatch');
+});
+
+llmRadio.addEventListener('click', function () {
+    currentUrl = urls['LLMJudge'];
+    updateTable(currentUrl);
+    updateCautionMessage('LLMJudge');
+});
+
+gpt35Radio.addEventListener('click', function () {
+    currentUrl = urls['GPT3.5Judge'];
+    updateTable(currentUrl);
+    updateCautionMessage('LLMJudge');
+});
+
+
+rougeRadio.addEventListener('click', function () {
+    currentUrl = urls['ROUGE'];
+    updateTable(currentUrl);
+    updateCautionMessage('ROUGE');
+});
+
+f1Radio.addEventListener('click', function () {
+    currentUrl = urls['F1'];
+    updateTable(currentUrl);
+    updateCautionMessage('ROUGE');
+});
+
+updateCautionMessage('ExactMatch');
